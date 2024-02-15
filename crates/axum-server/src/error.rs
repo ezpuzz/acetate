c79fe7e882513d88577717339ed767180d155c19
Emory Petermann <emory@onlyfor.us>
2024-02-15 13:09:05 -0800

    comment out unused errors

--- crates/axum-server/src/error.rs
+++ crates/axum-server/src/error.rs
@@ -1,10 +1,10 @@
-use axum::http::header::WWW_AUTHENTICATE;
-use axum::http::{HeaderMap, HeaderValue, StatusCode};
+// use axum::http::header::WWW_AUTHENTICATE;
+use axum::http::{/*HeaderMap, HeaderValue,*/ StatusCode};
 use axum::response::IntoResponse;
-use axum::Json;
+// use axum::Json;
 use sqlx::error::DatabaseError;
-use std::borrow::Cow;
-use std::collections::HashMap;
+// use std::borrow::Cow;
+// use std::collections::HashMap;
 
 /// A common error type that can be used throughout the API.
 ///
@@ -15,18 +15,17 @@ use std::collections::HashMap;
 /// message in a plain text body, or a JSON body in the case of `UnprocessableEntity`.
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    /// Return `401 Unauthorized`
-    #[error("authentication required")]
-    Unauthorized,
+    // /// Return `401 Unauthorized`
+    // #[error("authentication required")]
+    // Unauthorized,
 
-    /// Return `403 Forbidden`
-    #[error("user may not perform that action")]
-    Forbidden,
+    // /// Return `403 Forbidden`
+    // #[error("user may not perform that action")]
+    // Forbidden,
 
-    /// Return `404 Not Found`
-    #[error("request path not found")]
-    NotFound,
-
+    // /// Return `404 Not Found`
+    // #[error("request path not found")]
+    // NotFound,
     /// Return `422 Unprocessable Entity`
     ///
     /// This also serializes the `errors` map to JSON to satisfy the requirement for
@@ -37,13 +36,13 @@ pub enum Error {
     /// that the frontend can deal with, but I do admit sometimes I've just gotten lazy and
     /// returned a plain error message if there were few enough error modes for a route
     /// that the frontend could infer the error from the status code alone.
-    #[error("error in the request body")]
-    UnprocessableEntity {
-        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
-    },
+    // #[error("error in the request body")]
+    // UnprocessableEntity {
+    //     errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+    // },
 
-    #[error("a conflict has occurred")]
-    Conflict,
+    // #[error("a conflict has occurred")]
+    // Conflict,
 
     /// Automatically return `500 Internal Server Error` on a `sqlx::Error`.
     ///
@@ -91,30 +90,30 @@ impl Error {
     /// Multiple for the same key are collected into a list for that key.
     ///
     /// Try "Go to Usage" in an IDE for examples.
-    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
-    where
-        K: Into<Cow<'static, str>>,
-        V: Into<Cow<'static, str>>,
-    {
-        let mut error_map = HashMap::new();
+    // pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    // where
+    //     K: Into<Cow<'static, str>>,
+    //     V: Into<Cow<'static, str>>,
+    // {
+    //     let mut error_map = HashMap::new();
 
-        for (key, val) in errors {
-            error_map
-                .entry(key.into())
-                .or_insert_with(Vec::new)
-                .push(val.into());
-        }
+    //     for (key, val) in errors {
+    //         error_map
+    //             .entry(key.into())
+    //             .or_insert_with(Vec::new)
+    //             .push(val.into());
+    //     }
 
-        Self::UnprocessableEntity { errors: error_map }
-    }
+    //     Self::UnprocessableEntity { errors: error_map }
+    // }
 
     fn status_code(&self) -> StatusCode {
         match self {
-            Self::Unauthorized => StatusCode::UNAUTHORIZED,
-            Self::Forbidden => StatusCode::FORBIDDEN,
-            Self::NotFound => StatusCode::NOT_FOUND,
-            Self::Conflict => StatusCode::CONFLICT,
-            Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
+            // Self::Unauthorized => StatusCode::UNAUTHORIZED,
+            // Self::Forbidden => StatusCode::FORBIDDEN,
+            // Self::NotFound => StatusCode::NOT_FOUND,
+            // Self::Conflict => StatusCode::CONFLICT,
+            // Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
             Self::Sqlx(_) | Self::ES(_) | Self::Axum(_) | Self::Anyhow(_) => {
                 StatusCode::INTERNAL_SERVER_ERROR
             }
@@ -130,34 +129,33 @@ impl Error {
 impl IntoResponse for Error {
     fn into_response<'a>(self) -> axum::response::Response {
         match self {
-            Self::UnprocessableEntity { errors } => {
-                #[derive(serde::Serialize)]
-                struct Errors {
-                    errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
-                }
+            // Self::UnprocessableEntity { errors } => {
+            //     #[derive(serde::Serialize)]
+            //     struct Errors {
+            //         errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+            //     }
 
-                return (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response();
-            }
-            Self::Unauthorized => {
-                return (
-                    self.status_code(),
-                    // Include the `WWW-Authenticate` challenge required in the specification
-                    // for the `401 Unauthorized` response code:
-                    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
-                    //
-                    // The Realworld spec does not specify this:
-                    // https://realworld-docs.netlify.app/docs/specs/backend-specs/error-handling
-                    //
-                    // However, at Launchbadge we try to adhere to web standards wherever possible,
-                    // if nothing else than to try to act as a vanguard of sanity on the web.
-                    [(WWW_AUTHENTICATE, HeaderValue::from_static("Token"))]
-                        .into_iter()
-                        .collect::<HeaderMap>(),
-                    self.to_string(),
-                )
-                    .into_response();
-            }
-
+            //     return (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response();
+            // }
+            // Self::Unauthorized => {
+            //     return (
+            //         self.status_code(),
+            //         // Include the `WWW-Authenticate` challenge required in the specification
+            //         // for the `401 Unauthorized` response code:
+            //         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+            //         //
+            //         // The Realworld spec does not specify this:
+            //         // https://realworld-docs.netlify.app/docs/specs/backend-specs/error-handling
+            //         //
+            //         // However, at Launchbadge we try to adhere to web standards wherever possible,
+            //         // if nothing else than to try to act as a vanguard of sanity on the web.
+            //         [(WWW_AUTHENTICATE, HeaderValue::from_static("Token"))]
+            //             .into_iter()
+            //             .collect::<HeaderMap>(),
+            //         self.to_string(),
+            //     )
+            //         .into_response();
+            // }
             Self::Sqlx(ref e) => {
                 // TODO: we probably want to use `tracing` instead
                 // so that this gets linked to the HTTP request by `TraceLayer`.
@@ -197,10 +195,8 @@ impl IntoResponse for Error {
                 // TODO: we probably want to use `tracing` instead
                 // so that this gets linked to the HTTP request by `TraceLayer`.
                 dbg!("Generic error: {:?}", e);
-            }
-
-            // Other errors get mapped normally.
-            _ => (),
+            } // Other errors get mapped normally.
+              // _ => (),
         }
 
         (self.status_code(), self.to_string()).into_response()
