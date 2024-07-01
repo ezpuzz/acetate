a1bc6fc8437925f8ad03e35b0af214d13bd3fb30
Emory Petermann <emory@onlyfor.us>
2024-07-01 14:56:31 -0400

    linting

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,24 @@
 import asyncio
+import base64
 import os
-from flask import Flask, render_template, request, session, redirect, url_for
+import re
+
 import flask
-from flask_htmx import HTMX, make_response
 import flask_htmx
-from flask_sqlalchemy import SQLAlchemy
-
-from authlib.integrations.flask_client import OAuth
-from jinja2 import StrictUndefined
 import httpx
-
-from dotenv import load_dotenv
 import werkzeug
 import werkzeug.datastructures
-from pyroaring import BitMap
-import base64
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
 from elasticapm.contrib.flask import ElasticAPM
-import re
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_htmx import HTMX
+from flask_sqlalchemy import SQLAlchemy
+from jinja2 import StrictUndefined
+from pyroaring import BitMap
 from sqlalchemy.dialects.postgresql import insert
+
+from exeptions import LoggedOutError
 
 load_dotenv()
 
@@ -64,20 +65,20 @@ oauth = OAuth(app)
 
 def get_token():
     user = db.session.scalar(
-        db.select(User).where(User.discogs_user_id == session.get("user").get("id"))
+        db.select(User).where(User.discogs_user_id == session.get("user").get("id")),
     )
-    return dict(
-        oauth_token=user.discogs_oauth_token,
-        oauth_token_secret=user.discogs_oauth_token_secret,
-    )
+    return {
+        "oauth_token": user.discogs_oauth_token,
+        "oauth_token_secret": user.discogs_oauth_token_secret,
+    }
 
 
 oauth.register(
     name="discogs",
     client_id=os.environ.get("DISCOGS_CLIENT_ID", None),
     client_secret=os.environ.get("DISCOGS_CLIENT_SECRET", None),
-    request_token_url="https://api.discogs.com/oauth/request_token",
-    access_token_url="https://api.discogs.com/oauth/access_token",
+    request_token_url="https://api.discogs.com/oauth/request_token",  # noqa: S106
+    access_token_url="https://api.discogs.com/oauth/access_token",  # noqa: S106
     authorize_url="https://www.discogs.com/oauth/authorize",
     fetch_token=get_token,
 )
@@ -89,7 +90,7 @@ def healthz():
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found():
     return flask.redirect(url_for("discover"))
 
 
@@ -115,13 +116,13 @@ async def discover():
         releases = tg.create_task(get_releases(args))
 
     filters = filters.result()
-    releases, page, pageSize, offset, hits = releases.result()
+    releases, page, page_size, offset, hits = releases.result()
 
     if htmx and not htmx.boosted:
         return render_template(
             "discover/results.jinja",
             **{
-                "pageSize": pageSize,
+                "pageSize": page_size,
                 "releases": releases,
                 "hits": hits,
                 "page": page,
@@ -134,7 +135,7 @@ async def discover():
     return render_template(
         "discover.jinja",
         **{
-            "pageSize": pageSize,
+            "pageSize": page_size,
             "releases": releases,
             "hits": hits,
             "page": page,
@@ -153,13 +154,13 @@ async def dig():
         releases = tg.create_task(get_releases(request.args, omit_hidden=False))
 
     filters = filters.result()
-    releases, page, pageSize, offset, hits = releases.result()
+    releases, page, page_size, offset, hits = releases.result()
 
     if htmx and not htmx.boosted:
         return render_template(
             "dig/results.jinja",
             **{
-                "pageSize": pageSize,
+                "pageSize": page_size,
                 "releases": releases,
                 "hits": hits,
                 "page": page,
@@ -171,7 +172,7 @@ async def dig():
     return render_template(
         "dig.jinja",
         **{
-            "pageSize": pageSize,
+            "pageSize": page_size,
             "releases": releases,
             "hits": hits,
             "page": page,
@@ -198,15 +199,16 @@ def want():
 
 
 async def get_filters():
-    filters = httpx.get(f"{AXUM_API}filters", timeout=10)
-    filters.raise_for_status()
-    return filters.json().get("aggregations")
+    async with httpx.AsyncClient() as client:
+        filters = await client.get(f"{AXUM_API}filters", timeout=10)
+        filters.raise_for_status()
+        return filters.json().get("aggregations")
 
 
-async def get_releases(params: werkzeug.datastructures.MultiDict, omit_hidden=True):
-    pageSize = int(params.get("pageSize", 5))
-    offset = int(params.get("offset", (int(params.get("page", 1)) - 1) * pageSize))
-    page = 1 + offset // pageSize
+async def get_releases(params: werkzeug.datastructures.MultiDict, *, omit_hidden=True):
+    page_size = int(params.get("pageSize", 5))
+    offset = int(params.get("offset", (int(params.get("page", 1)) - 1) * page_size))
+    page = 1 + offset // page_size
 
     actions = []
     if "user" in session and omit_hidden:
@@ -217,20 +219,22 @@ async def get_releases(params: werkzeug.datastructures.MultiDict, omit_hidden=Tr
                 == db.select(User.user_id)
                 .scalar_subquery()
                 .where(User.discogs_user_id == session.get("user").get("id")),
-            )
+            ),
         ).all()
 
     params = [
         p
         for p in [
             (
-                "hide",
-                base64.urlsafe_b64encode(BitMap.serialize(BitMap(actions))).decode(
-                    "UTF-8"
-                ),
-            )
-            if actions
-            else None,
+                (
+                    "hide",
+                    base64.urlsafe_b64encode(BitMap.serialize(BitMap(actions))).decode(
+                        "UTF-8",
+                    ),
+                )
+                if actions
+                else None
+            ),
             params.get("label") and ("field", "nested:labels.name"),
             params.get("label") and ("value", params["label"]),
             params.get("song") and ("field", "nested:tracklist.title"),
@@ -240,13 +244,15 @@ async def get_releases(params: werkzeug.datastructures.MultiDict, omit_hidden=Tr
             params.get("title") and ("field", "title"),
             params.get("title") and ("value", params.get("title")),
             ("field", "nested:labels.catno") if params.get("catno") else None,
-            ("value", re.sub(r"(.*?)\s*-?\s*(\d+)", r"\1 \2", params.get("catno")))
-            if params.get("catno")
-            else None,
+            (
+                ("value", re.sub(r"(.*?)\s*-?\s*(\d+)", r"\1 \2", params.get("catno")))
+                if params.get("catno")
+                else None
+            ),
             ("field", "nested:identifiers.value") if params.get("identifier") else None,
             ("value", params.get("identifier")) if params.get("identifier") else None,
             ("from", offset),
-            ("size", pageSize),
+            ("size", page_size),
             (
                 "videos_only",
                 "true" if params.get("videos_only") == "on" else "false",
@@ -285,19 +291,20 @@ async def get_releases(params: werkzeug.datastructures.MultiDict, omit_hidden=Tr
         if p
     ]
 
-    releases = httpx.get(
-        f"{AXUM_API}releases",
-        params=params,
-        timeout=10,
-    )
-    releases.raise_for_status()
-    releases = releases.json()
+    async with httpx.AsyncClient() as client:
+        releases = client.get(
+            f"{AXUM_API}releases",
+            params=params,
+            timeout=10,
+        )
+        releases.raise_for_status()
+        releases = releases.json()
 
-    hits = int(releases["hits"]["total"]["value"])
+        hits = int(releases["hits"]["total"]["value"])
 
-    releases = [{**r["_source"], "id": r["_id"]} for r in releases["hits"]["hits"]]
+        releases = [{**r["_source"], "id": r["_id"]} for r in releases["hits"]["hits"]]
 
-    return releases, page, pageSize, offset, hits
+        return releases, page, page_size, offset, hits
 
 
 async def hide_release(release_id):
@@ -321,7 +328,7 @@ async def hide():
     params = request.form.copy()
     params["pageSize"] = "1"
     params["offset"] = str(
-        int(request.form.get("from", 0)) + int(request.form.get("pageSize", 5)) - 1
+        int(request.form.get("from", 0)) + int(request.form.get("pageSize", 5)) - 1,
     )
 
     async with asyncio.TaskGroup() as tg:
@@ -330,12 +337,12 @@ async def hide():
         releases = tg.create_task(get_releases(params))
 
     filters = filters.result()
-    releases, page, pageSize, offset, hits = releases.result()
+    releases, page, page_size, offset, hits = releases.result()
 
     return render_template(
         "discover/hidden.jinja",
         **{
-            "pageSize": pageSize,
+            "pageSize": page_size,
             "releases": releases,
             "hits": hits,
             "page": page,
@@ -349,14 +356,15 @@ async def hide():
 @app.route("/thumb/<release_id>")
 def thumb(release_id):
     if "user" not in session:
-        raise Exception()
+        raise LoggedOutError
 
     return render_template(
         "image.jinja",
         release_id=release_id,
         src=(
             oauth.discogs.get(
-                f"https://api.discogs.com/releases/{release_id}", timeout=5
+                f"https://api.discogs.com/releases/{release_id}",
+                timeout=5,
             )
             .json()
             .get("thumb")
@@ -367,7 +375,7 @@ def thumb(release_id):
 @app.route("/prices/<release_id>")
 def prices(release_id):
     if "user" not in session:
-        raise Exception()
+        raise LoggedOutError
 
     prices = oauth.discogs.get(
         f"https://api.discogs.com/marketplace/price_suggestions/{release_id}",
@@ -405,12 +413,14 @@ def prices(release_id):
 def wants():
     username = session["user"]["username"]
     wants = oauth.discogs.get(
-        f"https://api.discogs.com/users/{username}/wants", timeout=5
+        f"https://api.discogs.com/users/{username}/wants",
+        timeout=5,
     )
     wants.raise_for_status()
     wants = wants.json()
     return render_template(
-        "wants.jinja", **{"pageSize": 25, "wants": wants, **request.args}
+        "wants.jinja",
+        **{"pageSize": 25, "wants": wants, **request.args},
     )
 
 
@@ -440,10 +450,10 @@ def auth():
     )
     stmt = stmt.on_conflict_do_update(
         constraint="users_discogs_user_id_key",
-        set_=dict(
-            discogs_oauth_token=token["oauth_token"],
-            discogs_oauth_token_secret=token["oauth_token_secret"],
-        ),
+        set_={
+            "discogs_oauth_token": token["oauth_token"],
+            "discogs_oauth_token_secret": token["oauth_token_secret"],
+        },
     )
     db.session.execute(stmt)
     db.session.commit()
